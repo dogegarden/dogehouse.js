af06420937d4333b540d69e15f264ea28695c73a
Ilya Maximov <mail@overlisted.net>
2021-03-13 23:14:21 +0100

    add a more fancy logger

--- example.js
+++ example.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 
 const connect = require("./connect");
 
+const logger = (direction, opcode, data) => {
+  console.info(
+    `${direction.toUpperCase().padEnd(3, " ")} "${opcode}"\n${data ? JSON.stringify(data, null, 2) : ""}\n`
+  );
+}
+
 const main = async () => {
   try {
     const connection = await connect(
       process.env.DOGEHOUSE_TOKEN,
       process.env.DOGEHOUSE_REFRESH_TOKEN,
-      { logger: console.log }
+      { logger }
     );
     console.log(await connection.fetch("get_top_public_rooms", { cursor: 0 }))
   } catch(e) {
