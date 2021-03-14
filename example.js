eb3914dc1dd105689d0c1dedbd092b4ec3520946
Ilya Maximov <mail@overlisted.net>
2021-03-14 03:06:52 +0100

    write a simple chat-only client as an example

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const readline = require("readline");
 const connect = require("./connect");
 
 const logger = (direction, opcode, data, fetchId, raw) => {
@@ -13,9 +14,36 @@ const main = async () => {
     const connection = await connect(
       process.env.DOGEHOUSE_TOKEN,
       process.env.DOGEHOUSE_REFRESH_TOKEN,
-      { logger }
+      {}
     );
-    console.log(await connection.fetch("get_top_public_rooms", { cursor: 0 }))
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: `${connection.user.displayName} > `
+    })
+
+    const { rooms } = await connection.fetch("get_top_public_rooms", { cursor: 0 });
+    const theRoom = rooms[0];
+
+    console.log(`=> joining room "${theRoom.name}" (${theRoom.numPeopleInside} people)`);
+    await connection.fetch("join_room", { roomId: theRoom.id }, "join_room_done");
+
+    rl.prompt();
+    rl.on("line", async input => {
+      const message = { tokens: [{ t: "text", v: input }], whisperTo: [] };
+      await connection.send("send_room_chat_msg", message);
+    })
+
+    connection.addListener("new_chat_msg", async ({ userId, msg }) => {
+      const text = msg.tokens.map(it => it.v).reduce((a, b) => a + b);
+      if(userId !== connection.user.id) {
+        process.stdout.cursorTo(0);
+        console.log(`${msg.displayName} > ${text}`);
+      }
+
+      rl.prompt();
+    });
   } catch(e) {
     if(e.code === 4001) console.error("invalid token!");
   }
