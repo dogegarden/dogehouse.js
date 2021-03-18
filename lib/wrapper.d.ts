6bc2c0c354cf4b54eb366aa49ff5095b5c1973d2
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-03-18 15:08:35 +0100

    Compiled

--- lib/wrapper.d.ts
+++ lib/wrapper.d.ts
@@ -4,5 +4,8 @@ export declare const wrap: (connection: Connection) => {
     getTopPublicRooms: () => Promise<Room[]>;
     joinRoom: (id: UUID) => Promise<void>;
     sendRoomChatMsg: (ast: MessageToken[], whisperedTo?: string[]) => Promise<void>;
-    leaveRoom: (): Promise<{ roomId: UUID }> => connection.fetch("leave_room", {}, "you_left_room"),
+    leaveRoom: () => Promise<{
+        roomId: UUID;
+    }>;
+    listenForChatMsg: (callback: any) => void;
 };
