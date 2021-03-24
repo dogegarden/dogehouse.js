a82b8eb3308d89cb34fdd92433a24dcaf4af1b7d
HoloPanio <30759238+HoloPanio@users.noreply.github.com>
2021-03-24 01:29:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const UserController = require('../dhjs/src/controllers/UserController');
-
 module.exports = {
 	Client: require('./src/Client'),
-
-	MessageController: require('./src/controllers/MessageController'),
-	RoomController: require('./src/controllers/RoomController'),
-	UserController: require('./src/controllers/UserController')
 };
